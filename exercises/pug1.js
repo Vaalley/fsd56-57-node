7745bae146cbc07f7a68913f657f45164a154e81
vaalley <vaaleyou@gmail.com>
2025-05-15 09:56:09 +0200

    chore: remove unnecessary function

--- exercises/pug1.js
+++ exercises/pug1.js
@@ -1,14 +1,10 @@
 const pug = require('pug');
 const path = require('path');
 
-function renderFile(templatePath, data) {
-  return pug.renderFile(templatePath, data);
-}
-
 const template = path.join(__dirname, 'admin-message.pug');
 
 console.log('Rendu avec isAdmin: true');
-console.log(renderFile(template, { user: { isAdmin: true } }));
+console.log(pug.renderFile(template, { user: { isAdmin: true } }));
 
 console.log('\nRendu avec isAdmin: false');
-console.log(renderFile(template, { user: { isAdmin: false } }));
+console.log(pug.renderFile(template, { user: { isAdmin: false } }));
